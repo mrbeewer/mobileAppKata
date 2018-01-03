c75a777cbf7cb124df0fea7bb4c0388b161e2f2a
David Beeler <david.beeler@gmail.com>
2018-01-03 11:43:03 -0500

    make change functional, does not have knowledge of internal coin count

--- Vending Machine/Vending Machine/Logic/Machine.swift
+++ Vending Machine/Vending Machine/Logic/Machine.swift
@@ -14,26 +14,23 @@ class Machine {
 
     // MARK: - Variables
 
-    // TODO:
     // current state of the system
     // current amount of money in the system (reserves)
     // inventory
     // current amount of money inserted into the system
     //
-    private var moneyInserted: Double = 0.0
-    private var moneyAvailableTotal: Double = 0.0
-    private var moneyAvailable: MoneyCollection
+    private var coinsInserted: Double = 0.0
+    private var coinsInMachine: MoneyCollection
     private var coinReturn: MoneyCollection
 
     private var display: UILabel
 
     // MARK: - Methods
 
-    init(moneyAvailable: MoneyCollection, display: UILabel) {
+    init(coinsInMachine: MoneyCollection, display: UILabel) {
         print("Status: Initialize Machine")
-        self.moneyInserted = 0.0
-        self.moneyAvailableTotal = moneyAvailable.total()
-        self.moneyAvailable = moneyAvailable
+        self.coinsInserted = 0.0
+        self.coinsInMachine = coinsInMachine
 
         self.coinReturn = MoneyCollection(quarters: 0, dimes: 0, nickels: 0, pennies: 0)
 
@@ -46,55 +43,89 @@ class Machine {
     }
 
     // MARK: - Getters
-    func totalMoneyIn() -> Double {
-        return self.moneyAvailableTotal
-    }
-
-    func totalMoneyInsertedIn() -> Double {
-        return self.moneyInserted
-    }
-
+    
+    /// Gets the money in the coin return as a MoneyCollection type.
     func moneyInCoinReturn() -> MoneyCollection {
         return self.coinReturn
     }
 
     // MARK: - Money Adjustments
-    /// Called when the user inserts money. Will validate and increment the inserted total.
+    /**
+     Method called when the user inserts money. Will validate and increment the inserted total.
+     
+     - Parameters:
+     - type: The type of coin that was inserted.
+     */
     func insertMoney(type: Coins.CoinTypes) {
         switch type {
         case .penny:
             sendToCoinReturn(type: type)
         default:
-            self.moneyInserted += type.rawValue
+            self.coinsInserted += type.rawValue
         }
 
         displayMoneyInserted()
     }
 
-    func sendToCoinReturn(type: Coins.CoinTypes) {
+    /**
+     Method to send the coin type to the coin return
+     
+     - Parameters:
+     - type: The type of coin being sent.
+     - multiple: Defaults to 1, but can be used to send multiple of the same coin.
+     */
+    func sendToCoinReturn(type: Coins.CoinTypes, multiple: Int = 1) {
         switch type {
         case .quarter:
-            self.coinReturn.quarters += 1
+            self.coinReturn.quarters += 1 * multiple
         case .dime:
-            self.coinReturn.dimes += 1
+            self.coinReturn.dimes += 1 * multiple
         case .nickel:
-            self.coinReturn.nickels += 1
+            self.coinReturn.nickels += 1 * multiple
         case .penny:
-            self.coinReturn.pennies += 1
+            self.coinReturn.pennies += 1 * multiple
+        }
+    }
+
+    /**
+     Method to make change based on the selected product and inserted money.
+     
+     - Parameters:
+     - product: The product being purchased.
+     */
+    func makeChange(product: Products) {
+        var change = self.coinsInserted - product.getPrice()
+
+        if change > 0 {
+            let numQuarters = Int(floor(change / Coins.CoinTypes.quarter.rawValue))
+            sendToCoinReturn(type: Coins.CoinTypes.quarter, multiple: numQuarters)
+            
+            change -= 0.25 * Double(numQuarters)
+            let numDime = Int(floor(change / Coins.CoinTypes.dime.rawValue))
+            sendToCoinReturn(type: Coins.CoinTypes.dime, multiple: numDime)
+            
+            change -= 0.10 * Double(numDime)
+            let numNickel = Int(floor(change / Coins.CoinTypes.nickel.rawValue))
+            sendToCoinReturn(type: Coins.CoinTypes.nickel, multiple: numNickel)
+            
+            // pennies are rejected when inserted
         }
     }
 
     // MARK: - Display Adjustments
+    /// Sets the display to the default text - "INSERT COIN"
     func displayDefault() {
         self.display.text = "INSERT COIN"
     }
 
+    /// Sets the display to the done state - "THANK YOU"
     func displayDone() {
         self.display.text = "THANK YOU"
     }
 
+    /// Sets the display to show the money inserted - "Inserted: $#.##"
     func displayMoneyInserted() {
-        self.display.text = String(format: "Inserted: $%.02f", self.moneyInserted)
+        self.display.text = String(format: "Inserted: $%.02f", self.coinsInserted)
     }
 
     /// Struct to hold the quantities of the coin options
--- Vending Machine/Vending MachineTests/MachineTests.swift
+++ Vending Machine/Vending MachineTests/MachineTests.swift
@@ -10,9 +10,22 @@ import XCTest
 
 class MachineTests: XCTestCase {
 
+    var machine: Machine!
+    var totalMoney: Double = 0.0
+
+    var display: UILabel = UILabel()
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        let coinsInMachine: Machine.MoneyCollection = Machine.MoneyCollection(quarters: 10,
+                                                                              dimes: 10,
+                                                                              nickels: 10,
+                                                                              pennies: 0)
+        totalMoney = 0.25 * 10.0 + 0.10 * 10.0 + 0.05 * 10.0 + 0.01 * 0.0
+
+        machine = Machine(coinsInMachine: coinsInMachine, display: display)
     }
 
     override func tearDown() {
@@ -20,16 +33,194 @@ class MachineTests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    /* Test if display is initialized with "INSERT COINS"
+     * Check: display.text == "INSERT COINS"
+     */
+    func testMachineInitialization() {
+        XCTAssert(display.text! == "INSERT COINS", "Display text should read 'INSERT COINS' when initialized.")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    /* Test if the provided money collection adds up to the correct total of money available
+     * Check: totalMoneyIn = 4.0
+     */
+    func testMachineMoneyAvailableTotal() {
+        XCTAssert(machine.totalMoneyIn() == totalMoney,
+                  "Total value of money in machine does not equal \(totalMoney)")
+
+        // display should still be 'INSERT COINS'
+        XCTAssert(display.text! == "INSERT COINS", "Display text should read 'INSERT COINS'.")
+    }
+
+    /* Test if the machine will accept the correct coin types
+     * Check: total + 0.25 = 0.25
+     * Check: total + 0.10 = 0.35
+     * Check: total + 0.05 = 0.40
+     * Check: total + 0.01 = 0.40 (pennies not allowed)
+     * Check: coinReturn = 1 penny, 0 quarters, 0 dimes, 0 nickels
+     * Check: display for each total
+     */
+    func testMachineValidCoinTypes() {
+        var totalInserted = 0.0
+        var totalInsertedInMachine = 0.0
+
+        machine.insertMoney(type: Coins.CoinTypes.quarter)
+        totalInserted += Coins.CoinTypes.quarter.rawValue
+        totalInsertedInMachine = machine.totalMoneyInsertedIn()
+        XCTAssert(totalInsertedInMachine == totalInserted,
+                  "Inserted a quarter, total (\(totalInsertedInMachine)) should be \(totalInserted).")
+        XCTAssert(display.text! == "Inserted: $0.25", "Display text is \(display.text!)," +
+            " should read 'Inserted: $0.25'.")
+
+        machine.insertMoney(type: Coins.CoinTypes.dime)
+        totalInserted += Coins.CoinTypes.dime.rawValue
+        totalInsertedInMachine = machine.totalMoneyInsertedIn()
+        XCTAssert(totalInsertedInMachine == totalInserted,
+                  "Inserted a dime, total (\(totalInsertedInMachine)) should be \(totalInserted).")
+        XCTAssert(display.text! == "Inserted: $0.35", "Display text is \(display.text!)," +
+            "  should read 'Inserted: $0.35'.")
+
+        machine.insertMoney(type: Coins.CoinTypes.nickel)
+        totalInserted += Coins.CoinTypes.nickel.rawValue
+        totalInsertedInMachine = machine.totalMoneyInsertedIn()
+        XCTAssert(totalInsertedInMachine == totalInserted,
+                  "Inserted a nickel, total (\(totalInsertedInMachine)) should be \(totalInserted).")
+        XCTAssert(display.text! == "Inserted: $0.40", "Display text is \(display.text!)," +
+            "  should read 'Inserted: $0.40'.")
+
+        machine.insertMoney(type: Coins.CoinTypes.penny)
+        // totalInserted should not change, we are returning pennies
+        totalInsertedInMachine = machine.totalMoneyInsertedIn()
+        XCTAssert(totalInsertedInMachine == totalInserted,
+                  "Inserted a penny, total (\(totalInsertedInMachine)) should be \(totalInserted).")
+        XCTAssert(display.text! == "Inserted: $0.40", "Display text is \(display.text!)," +
+            "  should read 'Inserted: $0.40'.")
+
+        XCTAssert(machine.moneyInCoinReturn().pennies == 1,
+                  "Inserted a penny, coin return should only contain a single penny.")
+        XCTAssert(machine.moneyInCoinReturn().quarters == 0,
+                  "Inserted a penny, coin return should only contain a single penny. Contained quarters.")
+        XCTAssert(machine.moneyInCoinReturn().dimes == 0,
+                  "Inserted a penny, coin return should only contain a single penny. Contained dimes.")
+        XCTAssert(machine.moneyInCoinReturn().nickels == 0,
+                  "Inserted a penny, coin return should only contain a single penny. Contained nickels.")
+    }
+
+    /* Test that machine returns correct change
+     * Check: insert 1.00, buy item of 1.00 -> return 0
+     */
+    func testMachineMakeChangeInsertedEqualsPrice() {
+        for _ in 1...4 {
+            machine.insertMoney(type: Coins.CoinTypes.quarter)
         }
-    }
 
+        let cola = Products(type: Products.VendingItemType.cola, price: 1.0, inventory: 10)
+
+        machine.makeChange(product: cola)
+        let emptyCoinReturn = Machine.MoneyCollection(quarters: 0, dimes: 0, nickels: 0, pennies: 0)
+        XCTAssert(machine.moneyInCoinReturn().total() == emptyCoinReturn.total(),
+                  "Inserted \(machine.totalMoneyInsertedIn()) and purchased cola for " +
+                  "\(cola.getPrice()). CoinReturn should be empty.")
+    }
+    
+    /* Test that machine returns correct change
+     * Check: insert 2.00, buy item of 1.00 -> return 4 quarters
+     */
+    func testMachineMakeChangeInsertedMoreThanPriceQuarters() {
+        for _ in 1...8 {
+            machine.insertMoney(type: Coins.CoinTypes.quarter)
+        }
+
+        let cola = Products(type: Products.VendingItemType.cola, price: 1.0, inventory: 10)
+
+        machine.makeChange(product: cola)
+        let correctCoinReturn = Machine.MoneyCollection(quarters: 4, dimes: 0, nickels: 0, pennies: 0)
+
+        XCTAssert(machine.moneyInCoinReturn().pennies == 0)
+        XCTAssert(machine.moneyInCoinReturn().quarters == 4)
+        XCTAssert(machine.moneyInCoinReturn().dimes == 0)
+        XCTAssert(machine.moneyInCoinReturn().nickels == 0)
+
+        XCTAssert(machine.moneyInCoinReturn().total() == correctCoinReturn.total(),
+                  "Inserted \(machine.totalMoneyInsertedIn()) and purchased cola for " +
+                  "\(cola.getPrice()). CoinReturn should be $1.00.")
+    }
+    
+    /* Test that machine returns correct change
+     * Check: insert 1.20, buy item of 1.00 -> return 2 dimes
+     */
+    func testMachineMakeChangeInsertedMoreThanPriceDimes() {
+        for _ in 1...4 {
+            machine.insertMoney(type: Coins.CoinTypes.quarter)
+        }
+        for _ in 1...2 {
+            machine.insertMoney(type: Coins.CoinTypes.dime)
+        }
+        
+        let cola = Products(type: Products.VendingItemType.cola, price: 1.0, inventory: 10)
+        
+        machine.makeChange(product: cola)
+        let correctCoinReturn = Machine.MoneyCollection(quarters: 0, dimes: 2, nickels: 0, pennies: 0)
+        
+        XCTAssert(machine.moneyInCoinReturn().quarters == 0)
+        XCTAssert(machine.moneyInCoinReturn().dimes == 2)
+        XCTAssert(machine.moneyInCoinReturn().nickels == 0)
+        XCTAssert(machine.moneyInCoinReturn().pennies == 0)
+        
+        XCTAssert(machine.moneyInCoinReturn().total() == correctCoinReturn.total(),
+                  "Inserted \(machine.totalMoneyInsertedIn()) and purchased cola for " +
+            "\(cola.getPrice()). CoinReturn should be $0.20.")
+    }
+    
+    /* Test that machine returns correct change
+     * Check: insert 1.05, buy item of 1.00 -> return 1 nickel
+     */
+    func testMachineMakeChangeInsertedMoreThanPriceNickel() {
+        for _ in 1...4 {
+            machine.insertMoney(type: Coins.CoinTypes.quarter)
+        }
+        for _ in 1...1 {
+            machine.insertMoney(type: Coins.CoinTypes.nickel)
+        }
+        
+        let cola = Products(type: Products.VendingItemType.cola, price: 1.0, inventory: 10)
+        
+        machine.makeChange(product: cola)
+        let correctCoinReturn = Machine.MoneyCollection(quarters: 0, dimes: 0, nickels: 1, pennies: 0)
+        
+        XCTAssert(machine.moneyInCoinReturn().quarters == 0)
+        XCTAssert(machine.moneyInCoinReturn().dimes == 0)
+        XCTAssert(machine.moneyInCoinReturn().nickels == 1)
+        XCTAssert(machine.moneyInCoinReturn().pennies == 0)
+        
+        XCTAssert(machine.moneyInCoinReturn().total() == correctCoinReturn.total(),
+                  "Inserted \(machine.totalMoneyInsertedIn()) and purchased cola for " +
+            "\(cola.getPrice()). CoinReturn should be $0.05.")
+    }
+    
+    /* Test that machine returns correct change
+     * Check: insert 1.66, buy item of 1.00 -> return 2 quarters, 1 dime, 1 nickel, 1 penny
+     */
+    func testMachineMakeChangeInsertedMoreThanPriceMixed() {
+        for _ in 1...6 {
+            machine.insertMoney(type: Coins.CoinTypes.quarter)
+        }
+        
+        machine.insertMoney(type: Coins.CoinTypes.penny)
+        machine.insertMoney(type: Coins.CoinTypes.dime)
+        machine.insertMoney(type: Coins.CoinTypes.nickel)
+        
+        let cola = Products(type: Products.VendingItemType.cola, price: 1.0, inventory: 10)
+        
+        machine.makeChange(product: cola)
+        let correctCoinReturn = Machine.MoneyCollection(quarters: 2, dimes: 1, nickels: 1, pennies: 1)
+        
+        XCTAssert(machine.moneyInCoinReturn().quarters == 2)
+        XCTAssert(machine.moneyInCoinReturn().dimes == 1)
+        XCTAssert(machine.moneyInCoinReturn().nickels == 1)
+        XCTAssert(machine.moneyInCoinReturn().pennies == 1)
+        
+        XCTAssert(machine.moneyInCoinReturn().total() == correctCoinReturn.total(),
+                  "Inserted \(machine.totalMoneyInsertedIn()) and purchased cola for " +
+            "\(cola.getPrice()). CoinReturn should be $0.66.")
+    }
 }
